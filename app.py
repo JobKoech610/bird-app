0459334cc1d09c2da3c4b035aad2d84937ab187f
Job koech <jobkoech610@gmail.com>
2023-06-30 15:04:02 +0300

    edited

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_restful import Api, Resource
 from models import db, Bird
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql://job:mqSElSuLMSDWUwcPn6jcxCcSCoLhKLEk@dpg-cieab7lgkuvlk1mojff0-a.oregon-postgres.render.com/bird_app_lb9a')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
